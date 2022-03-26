b7144f02a3d767ab72599a789e551257b9dd57d1
igorroncevic <ironcevic98@gmail.com>
2022-03-26 17:10:15 +0100

    bumped Next.js version to 12.1

--- next.config.js
+++ next.config.js
@@ -11,11 +11,22 @@ module.exports = withPWA({
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    // Docs: https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.optimization.runtimeChunk = "single";
+
+        // Important: return the modified config
+        return config
+    },
     env: {
         JWT_SECRET: "djhfghbdsgrasklkajsdgf",
         REACT_APP_SOLANA_NETWORK: "devnet",
         REACT_APP_SOLANA_RPC_HOST: "https://explorer-api.devnet.solana.com"
     },
+    images: {
+        domains: ["res.cloudinary.com", "localhost"],
+    },
     reactStrictMode: true,
     staticPageGenerationTimeout: 120,
+    swcMinify: true // using Terser with SWC for faster minifies
 })
