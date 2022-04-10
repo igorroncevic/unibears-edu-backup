0f7f89652e330c34f5e7409978842b8d1ee677b2
Igor Rončević <57319163+igorroncevic@users.noreply.github.com>
2022-04-10 20:01:36 +0200

    Improving Connect Wallet button styles (#20)

---
+++ components/Common/Spinner.js
@@ -0,0 +1,11 @@
+import { Spinner as BootstrapSpinner } from "react-bootstrap";
+
+const Spinner = () => {
+	return (
+		<BootstrapSpinner animation="border" role="status" variant="light" size="sm">
+			<span className="visually-hidden">Loading...</span>
+		</BootstrapSpinner>
+	)
+}
+
+export default Spinner;
